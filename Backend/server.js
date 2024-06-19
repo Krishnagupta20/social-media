273f0683b127c2a1df4ec17016a03f6bdd0348ac
Krishna Gupta <krishna.g@crib.in>
2024-06-19 12:56:19 +0530

    production

--- Backend/server.js
+++ Backend/server.js
@@ -7,12 +7,10 @@ import postRoutes from "./routes/postRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import {v2 as cloudinary} from "cloudinary"
 import { app, server } from "./socket/socket.js";
-import job from "./cron/cron.js";
 import path from "path";
 
 dotenv.config();
 connectDB();
-job.start();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
@@ -32,6 +30,12 @@ app.use("/api/users",userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () =>
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/social/dist")))
+  app.get("*",(req, res)=>{
+    res.sendFile(path.resolve(__dirname, "social", "dist", "index.html"))
+  })
+}
+server.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
 );
